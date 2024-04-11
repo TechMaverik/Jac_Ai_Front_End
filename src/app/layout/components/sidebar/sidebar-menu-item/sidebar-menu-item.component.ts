8d13ef3d656fe8f2a9d66a5b4a7d38f9e6de281a
DzoeL123 <47805022+DzoeL123@users.noreply.github.com>
2024-04-11 13:14:41 +0000

    Changes:
    - Create sidebar menu and menu item components

---
+++ src/app/layout/components/sidebar/sidebar-menu-item/sidebar-menu-item.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'ja-sidebar-menu-item',
+  templateUrl: './sidebar-menu-item.component.html',
+  styleUrl: './sidebar-menu-item.component.scss'
+})
+export class SidebarMenuItemComponent {
+
+}

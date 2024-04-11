ba1587a73ac0e8d86b42fc807adcb0e5e17099e8
DzoeL123 <47805022+DzoeL123@users.noreply.github.com>
2024-04-11 13:26:22 +0000

    Changes:
    - Setup library for UI components

---
+++ src/libs/shared/ui/src/lib/ja-card/ja-card.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'lib-ja-card',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './ja-card.component.html',
+  styleUrl: './ja-card.component.css',
+})
+export class JaCardComponent {}

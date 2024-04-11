182eb8a6fe9cc104e50e36f2ed52026b7767f2a3
DzoeL123 <47805022+DzoeL123@users.noreply.github.com>
2024-04-11 13:19:57 +0000

    Changes:
    - Setup basic navigation routes and environments

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,31 @@
 import { Routes } from '@angular/router';
+import { LayoutComponent } from './layout/layout.component';
+import { DashboardComponent } from './layout/components/dashboard/dashboard.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'jac-ai',
+        component: LayoutComponent,
+        children: [
+            {
+                path: 'dashboard',
+                loadChildren: () => import('./layout/components/dashboard/dashboard.module').then((m) => m.DashboardModule)
+            },
+            {
+                path: '',
+                redirectTo: 'dashboard',
+                pathMatch: 'full'
+            },
+        ]
+    },
+    {
+        path: '',
+        redirectTo: 'jac-ai',
+        pathMatch: 'full'
+    },
+    {
+        path: '**',
+        redirectTo: 'jac-ai',
+        pathMatch: 'full'
+    }
+];
